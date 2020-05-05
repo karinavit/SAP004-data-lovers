bb8c476bfdf0c77c6dbf0e12059a5be0ed368e45
karinavit <ka.vitangelo@gmail.com>
2020-05-05 19:40:21 -0300

    testes iniciais para importar funções e fazer os cards aparecerem na tela,  nos arquivos data.js e  main.js

--- src/data.js
+++ src/data.js
@@ -1,10 +1,18 @@
 // funções de exemplo
 
-export const example = () => {
+/* export const example = () => {
   return 'example';
 };
 
 export const anotherExample = () => {
   return 'OMG';
-};
+}; */
 
+export const filterLol = () => {
+  array.function (key , type, id, image);
+}
+    return function
+  
+ 
+
+  //return 'example'
