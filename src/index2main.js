a15034136d92b7eff45f04238bcbd8d8c0462165
karinavit <ka.vitangelo@gmail.com>
2020-05-15 20:43:04 -0300

    Corrigido bug de armazenamento  da primeira página de entrada, separados arquivos de js e index, iniciado o modal

--- src/index2main.js
+++
@@ -1,5 +0,0 @@
-function setName() {
-    console.log('RODOU NO LOAD', sessionStorage.getItem('summonerName'));
-    console.log(document.getElementById('hello').value);
-    document.getElementById('hello').innerHTML = sessionStorage.getItem('summonerName');
-  }
