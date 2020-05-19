29dc308923657bf4e198bf83496b20032cfd6c62
karinavit <ka.vitangelo@gmail.com>
2020-05-19 11:27:50 -0300

    Inicio da construção das funções de testes

--- src/data.js
+++ src/data.js
@@ -1,5 +1,6 @@
-export const orderAz =(lolArchive)=>lolArchive.sort((a, b) => {
-  return (a.name < b.name) ? - 1 : 1
+export const orderAz =(lolArchive)=>lolArchive.sort((data) => {
+
+  return (data.name < data.name) ? - 1 : 1
 });
 export const orderZa =(lolArchive)=>lolArchive.sort((a, b) => {
   return (a.name > b.name) ? - 1 : 1
@@ -7,7 +8,7 @@ export const orderZa =(lolArchive)=>lolArchive.sort((a, b) => {
 
 export function filterLol(lolArchive, typeChampions) {
   return lolArchive.filter(item => item.tags.includes(typeChampions));
-  }; 
+  }
 
  export function searchName (lolArchive, search) { return lolArchive.filter(item =>item.name.toLowerCase().includes(search.toLowerCase())); }
 
