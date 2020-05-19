b55651e09feac3e40c65358661c545df114abf62
Adriana Fernando <adriana.drih@gmail.com>
2020-05-19 16:44:26 -0300

    Iniciando testes das funções

--- src/data.js
+++ src/data.js
@@ -1,15 +1,27 @@
-export const orderAz =(lolArchive)=>lolArchive.sort((data) => {
+/* export const orderAz =(lolArchive)=>lolArchive.sort((data) => {
 
   return (data.name < data.name) ? - 1 : 1
-});
-export const orderZa =(lolArchive)=>lolArchive.sort((a, b) => {
+}); */
+
+export const orderAz =(lolArchive)=>{
+  const novoArray = [...lolArchive]
+  return novoArray.sort((a, b) => {
+  return (a.name < b.name) ? - 1 : 1
+})};
+
+export const orderZa =(lolArchive)=>{
+  const novoArray = [...lolArchive]
+  return novoArray.sort((a, b) => {
   return (a.name > b.name) ? - 1 : 1
-}); 
+})};
+/* export const orderZa =(lolArchive)=>lolArchive.sort((a, b) => {
+  return (a.name > b.name) ? - 1 : 1
+});  */
 
 export function filterLol(lolArchive, typeChampions) {
   return lolArchive.filter(item => item.tags.includes(typeChampions));
-  }
+  };
 
- export function searchName (lolArchive, search) { return lolArchive.filter(item =>item.name.toLowerCase().includes(search.toLowerCase())); }
+export function searchName (lolArchive, search) { return lolArchive.filter(item =>item.name.toLowerCase().includes(search.toLowerCase())); }
 
 export const calc = (filtercalc,data) => Math.round(((filtercalc.length) *100) / (data.length))
